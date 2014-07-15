87cc33e29f70f9f8bff9a151b3dd20359062a561
wakelank <wakelank@gmail.com>
2014-07-15 10:18:41 -0400

    working on heroku

--- app/assets/javascripts/views/doll_view.js
+++ app/assets/javascripts/views/doll_view.js
@@ -6,29 +6,35 @@ var DollView = Backbone.View.extend({
   render: function(){
 
     var doll = this.model;
-    var doll_image = s.circle(doll.get("xPos"), doll.get("yPos"), 50);
-    doll_image.attr({
-      fill: this.model.attributes.color,
-      id: doll.get("name"),
-      class: "doll"
-    });
+    var g = s.group();
+    // var doll_image = s.circle(doll.get("xPos"), doll.get("yPos"), 50);
+    // doll_image.attr({
+    //   fill: this.model.attributes.color,
+    //   id: doll.get("name"),
+    //   class: "doll"
+    // });
 
-    var moveFunc = function( dx, dy, posx, posy){
-      //TODO this  y-100 is to take into account the header
-      //need to use the x, y values of the Snap paper, not 
-      //page.  maybe this.arr("x"), this.attr("y")
-      this.attr({ cx: posx, cy: posy-100})
+    Snap.load("assets/" + doll.attributes.file_name, function(f){
+      var doll_image = g.append(f);
+      var matrix = doll_image.transform().globalMatrix;
+    })
 
-      var itemsArr = doll_items.models;
-  //TODO see if an each function would work
-      for (var i = 0; i < itemsArr.length; i++ ){
-        itemsArr[i].attributes.attr({ x: posx, y: posy-100 })
-      }
+  //   var moveFunc = function( dx, dy, posx, posy){
+  //     //TODO this  y-100 is to take into account the header
+  //     //need to use the x, y values of the Snap paper, not 
+  //     //page.  maybe this.arr("x"), this.attr("y")
+  //     this.attr({ cx: posx, cy: posy-100})
+
+  //     var itemsArr = doll_items.models;
+  // //TODO see if an each function would work
+  //     for (var i = 0; i < itemsArr.length; i++ ){
+  //       itemsArr[i].attributes.attr({ x: posx, y: posy-100 })
+  //     }
   
 
-    }
+  //   }
 
-    doll_image.drag(moveFunc);
+    // doll_image.drag(moveFunc);
 
     return this;
   }
